e676ebe92de5025d4c9e987b04b7278efb1acecd
Michelle <Michellew94086@gmail.com>
2019-07-10 11:46:32 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('date').innerHTML = new Date().toDateString();
+document.getElementById("date").style.backgroundColor = "white "
+// alert("Welcome to my website!")
+
+function buttonReset() {
+  document.getElementById("frm1").reset();
+}
